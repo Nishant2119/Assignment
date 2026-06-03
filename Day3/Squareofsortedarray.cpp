ae5277f89043c1ceaabb6ba2b27811f4d656bf90
Nishant2119 <nishantbenares@gmail.com>
2026-06-03 22:56:22 +0530

    Square of sorted array solution

---
+++ Day3/Squareofsortedarray.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+ public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        vector<int> ans(nums.size(),0);
+        int l = 0, r = nums.size()-1;
+        int i = nums.size()-1;
+        while(l <= r)
+        {
+            if(abs(nums[l]) >= abs(nums[r]))
+            {
+                ans[i--] = nums[l]*nums[l];
+                l++;
+            }
+            else 
+            {
+                ans[i--] = nums[r]*nums[r];
+                r--;
+            }
+        }
+        return ans;
+    }
+};
