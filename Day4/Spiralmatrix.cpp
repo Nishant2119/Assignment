1efa7b207c242f4fe1bd952fc13c446d5a201500
Nishant2119 <nishantbenares@gmail.com>
2026-06-04 22:38:38 +0530

    Spiral matrix solution

---
+++ Day4/Spiralmatrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
+
+        vector<int> result;
+
+        while (top <= bottom && left <= right)
+        {
+            // top row
+            for (int i = left; i <= right; i++)
+                result.push_back(matrix[top][i]);
+            top++;
+
+            // right column
+            for (int i = top; i <= bottom; i++)
+                result.push_back(matrix[i][right]);
+            right--;
+
+            // bottom row
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                    result.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+
+            // left column
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                    result.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+
+        return result;
+    }
+};
