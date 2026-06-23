c192a7aa4b06a622e710611a19b37db173568033
Nishant2119 <nishantbenares@gmail.com>
2026-06-23 20:57:05 +0530

    Binary tree inorder traversal solution

---
+++ Week4/Day2/BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        if(root!=nullptr)
+        {
+            inorderTraversal(root->left);
+            ans.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return ans;
+    }
+};
